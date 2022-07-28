a7cb49d4ecd9cdd371b6bf272f074bd7b898870b
Charlie Eddus <charlieeddus@gmail.com>
2022-07-28 10:37:03 +0200

    Add grouping and anchors

--- podcast-search/script.js
+++ podcast-search/script.js
@@ -1,3 +1,4 @@
+const anchors = document.getElementById("anchors");
 const list = document.getElementById("items");
 const duplicatesList = document.getElementById("duplicates");
 const count = document.getElementsByClassName("count")[0];
@@ -61,7 +62,7 @@ function fetchPodcasts() {
         fetchPodcasts();
       } else {
         let duplicates = []
-        const sortedPodcasts = podcasts
+        const filteredPodcasts = podcasts
           .filter(podcast => {
             const unwantedGenres = [
               "Business",
@@ -83,7 +84,7 @@ function fetchPodcasts() {
               "Fiction"
             ];
 
-            return !unwantedGenres.includes(podcast.genres[0])
+            return !unwantedGenres.some(r => podcast.genres.includes(r))
           })
           .filter(podcast => {
             if (!recent.checked) { return true }
@@ -98,11 +99,16 @@ function fetchPodcasts() {
           })
           .sort((a, b) => (a.releaseDate < b.releaseDate) ? 1 : -1)
 
-        count.innerHTML = sortedPodcasts.length + " podcasts"
+        const groupedPodcasts = groupBy(filteredPodcasts, podcast => podcast.primaryGenreName);
 
-        for (const podcast of sortedPodcasts) {
-          addElement(podcast);
+        for (const group of groupedPodcasts) {
+          createGroupAnchor(group);
+          createGroup(group);
         }
+
+        console.log(groupedPodcasts)
+
+        count.innerHTML = "Found " + filteredPodcasts.length + " podcasts in " + groupedPodcasts.size + " categories"
 
         if (duplicates.length > 0) {
           duplicatesCount.innerHTML = duplicates.length + " possible duplicate" + (duplicates.length > 1 ? "s" : "")
@@ -113,6 +119,55 @@ function fetchPodcasts() {
         }
       }
     });
+}
+
+function groupBy(list, keyGetter) {
+  const map = new Map();
+
+  list.forEach((item) => {
+       const key = keyGetter(item);
+       const collection = map.get(key);
+       if (!collection) {
+           map.set(key, [item]);
+       } else {
+           collection.push(item);
+       }
+  });
+
+  return map;
+}
+
+function createGroupAnchor(group) {
+    const anchor = document.createElement("a");
+    anchor.setAttribute("class", "group-anchor");
+    anchor.setAttribute("href", `#${group[0]}`);
+    anchor.innerText = group[0];
+
+    anchors.appendChild(anchor);
+}
+
+function createGroup(group) {
+  const groupElement = document.createElement("div");
+  groupElement.setAttribute("class", "group")
+
+  const title = document.createElement("p");
+  title.setAttribute("class", "title");
+  title.setAttribute("id", group[0]); 
+  title.innerText = group[0];
+
+  const items = document.createElement("div");
+  items.setAttribute("class", "items");
+
+  groupElement.appendChild(title);
+  groupElement.appendChild(items);
+
+  const podcasts = group[1].sort((a, b) => (a.releaseDate < b.releaseDate) ? 1 : -1)
+
+  for (const podcast of podcasts) {
+    items.appendChild(createElement(podcast));
+  }
+
+  list.appendChild(groupElement);
 }
 
 function createElement(podcast) {
@@ -133,7 +188,7 @@ function createElement(podcast) {
 
   const genre = document.createElement("p");
   genre.setAttribute("class", "genre");
-  genre.innerText = podcast.genres[0];
+  genre.innerText = podcast.primaryGenreName;
 
   const latestRelease = document.createElement("p");
   latestRelease.setAttribute("class", "latest-release");
